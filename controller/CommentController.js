d99a62d1a66ffd4e426fdd8dab741444b1981e14
Davi Batista <davideosmar11@gmail.com>
2023-04-17 17:20:26 -0300

    Comentários finalizados

--- controller/CommentController.js
+++ controller/CommentController.js
@@ -8,9 +8,8 @@ module.exports = {
         const {authorId, rating, text} = requisition.body
 
         const product = await Product.findByPk(productId)
-        const author = await Client.findByPk(authorId)
-        if(!product || !author){
-            response.send(`Product or author were not found!`)
+        if(!product){
+            return response.send(`Product or author were not found!`)
         }
         const [comments] = await Comment.findOrCreate({
             where: {authorId, rating, text}
@@ -22,17 +21,22 @@ module.exports = {
 
     },
     async index(requisition, response) {
-        const {productId, id} = requisition.params
+        const {productId} = requisition.params
         const product = await Product.findByPk(productId, {
             include: {association: "comments"}
         })
+        if(!product){
+            return response.send(`Product or author were not found!`)
+        }
         return response.json(product.comments)
     },
     async put(requisition, response) {
-        const {productId} = requisition.params
+        const {productId, id} = requisition.params
         const product = await Product.findByPk(productId)
-        if(!product){
-            response.send(`Product was not found! :(`)
+        const comment = await Comment.findByPk(id)
+
+        if(!product || !comment){
+            return response.send(`Product or comment were not found!`)
         }
         const {authorId, rating, text} = requisition.body
         await Comment.update(
@@ -46,10 +50,12 @@ module.exports = {
         return response.send(`Your comment has been succesfully edited!`)
     },
     async delete(requisition, response) {
-        const {productId} = requisition.params
+        const {productId, id} = requisition.params
         const product = await Product.findByPk(productId)
-        if(!product){
-            response.send(`Product was not found! :(`)
+        const comment = await Comment.findByPk(id)
+
+        if(!product || !comment){
+            return response.send(`Product or comment were not found!`)
         }
         await Comment.destroy({
             where:  {
